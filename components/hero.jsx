ee12e0074c56fb09cd14e8f9c3abf38a5906c097
Sandeeppanwr <166792354+Sandeeppanwr@users.noreply.github.com>
2025-05-02 12:11:50 +0700

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -42,7 +42,7 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
+          <Link href=https://www.google.com/search?q=kg+academy&oq=kg+aca&gs_lcrp=EgZjaHJvbWUqBwgBEAAYgAQyBggAEEUYOTIHCAEQABiABDIGCAIQBhhAMgcIAxAAGIAEMg0IBBAuGK8BGMcBGIAEMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMgoICBAAGAoYFhgeMggICRAAGBYYHtIBCDUxNTlqMGo0qAIAsAIB&sourceid=chrome&ie=UTF-8>
             <Button size="lg" variant="outline" className="px-8">
               Watch Demo
             </Button>
